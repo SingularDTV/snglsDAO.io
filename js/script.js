55ffd1a7d4a674fa0a67da11c2293ec11afd80c0
Troy Murray <troykaa@gmail.com>
2019-11-22 14:08:55 -0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,86 @@
+
+/*  mobile menu toggle */
+$('.menu-btn').on('click', function(){
+  if($(this).parents('body').is('.opened-menu') !== true) {
+    $('body').addClass('opened-menu');
+    $('.menu').addClass('opened');
+  }
+  else if($(this).parents('body').is('.opened-menu') === true){
+    $('body').removeClass('opened-menu');
+    $('.menu').removeClass('opened');
+  }
+});
+
+/* Counters */
+var $animation_elements = $('.dao__counter');
+var $window = $(window);
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
+
+function check_if_in_view() {
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+
+  $.each($animation_elements, function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+
+    //check to see if this current container is within viewport
+    if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) {
+      $element.addClass('in-view');
+      $(this).prop('Counter', $(this).attr( "data-from" )).animate({
+          Counter: $(this).attr( "data-to" )
+      }, {
+          duration: 4000,
+          easing: 'swing',
+          step: function (now) {
+              $(this).text(Math.ceil(now));
+          }
+      });
+    } else {
+      $element.removeClass('in-view');
+    }
+  });
+}
+
+ /* Scroll to top */
+ $('#scrollTop').click(function() {
+    $('body,html').animate({
+        scrollTop : 0
+    }, 1000);
+});
+
+/* Floating Box */
+/* Common box status - visible */
+if (!(localStorage.getItem('popupStatus'))) {
+        localStorage.setItem('popupStatus', 'visible');
+    }
+/* Close click event */
+$( document ).ready(function() {
+    $("#floating_box_close").on("click", function() {
+        console.log("closed!");
+        localStorage.setItem('popupStatus', 'closed');
+        $('#floating_box').hide();
+    });
+});
+/* Hide box if it was closed previously */
+if (localStorage.getItem('popupStatus') === 'closed') {
+        $('#floating_box').hide();
+    }
+
+/* Show box if it was enabled previously */
+if (localStorage.getItem('popupStatus') === 'visible') {
+        jQuery('#floating_box').show();
+    }
+/*uncomment this line to show the box again*/
+/*jQuery('#floating_box').show();*/
+
+/* MODAL FORM */
+$('.modal-form-toggle').on('click', function(e) {
+  e.preventDefault();
+  $('.modal').toggleClass('is-visible');
+});
